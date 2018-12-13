563c622c2d56b703a6f96718bc775e4c6bd11f56
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-13 21:54:57 +0100

    Add section information into the parsing

--- src/ByteLibLoader/PlatformLoaders/PE/OptionalHeader.cs
+++ src/ByteLibLoader/PlatformLoaders/PE/OptionalHeader.cs
@@ -181,6 +181,151 @@ namespace ByteLibLoader.PlatformLoaders.PE
         public uint NumberOfRvaAndSizes { get; set; }
 
         /// <summary>
+        /// Gets or sets the address of the export table.
+        /// </summary>
+        public uint ExportTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the export table.
+        /// </summary>
+        public uint SizeOfExportTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the import table.
+        /// </summary>
+        public uint ImportTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the import table.
+        /// </summary>
+        public uint SizeOfImportTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the resource table.
+        /// </summary>
+        public uint ResourceTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the resource table.
+        /// </summary>
+        public uint SizeOfResourceTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the exception table.
+        /// </summary>
+        public uint ExceptionTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the exception table.
+        /// </summary>
+        public uint SizeOfExceptionTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the certificate table.
+        /// </summary>
+        public uint CertificateTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the certificate table.
+        /// </summary>
+        public uint SizeOfCertificateTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the base relocation table.
+        /// </summary>
+        public uint BaseRelocationTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the base relocation table.
+        /// </summary>
+        public uint SizeOfBaseRelocationTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the debug information.
+        /// </summary>
+        public uint Debug { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the debug information.
+        /// </summary>
+        public uint SizeOfDebug { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the architecture data.
+        /// </summary>
+        public uint ArchitectureData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the architecture data.
+        /// </summary>
+        public uint SizeOfArchitectureData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the global pointer (has no size).
+        /// </summary>
+        public uint GlobalPtr { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the TLS table.
+        /// </summary>
+        public uint TlsTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the TLS table.
+        /// </summary>
+        public uint SizeOfTlsTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the load config table.
+        /// </summary>
+        public uint LoadConfigTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the load config table.
+        /// </summary>
+        public uint SizeOfLoadConfigTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the bound import.
+        /// </summary>
+        public uint BoundImport { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the bound import.
+        /// </summary>
+        public uint SizeOfBoundImport { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the import address table.
+        /// </summary>
+        public uint ImportAddressTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the import address table.
+        /// </summary>
+        public uint SizeOfImportAddressTable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the delay import descriptor.
+        /// </summary>
+        public uint DelayImportDescriptor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the delay import descriptor.
+        /// </summary>
+        public uint SizeOfDelayImportDescriptor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the CLR runtime header.
+        /// </summary>
+        public uint ClrRuntimeHJeader { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the CLR runtime header.
+        /// </summary>
+        public uint SizeOfClrRuntimeHJeaderr { get; set; }
+
+        /// <summary>
         /// Creates a new <see cref="OptionalHeader"/> instance from a stream.
         /// </summary>
         /// <param name="stream">The stream to read from.</param>
@@ -219,6 +364,36 @@ namespace ByteLibLoader.PlatformLoaders.PE
             header.SizeOfHeapCommit = (header.Magic == Hdr64Magic) ? stream.ReadUInt64() : stream.ReadUInt32();
             header.LoaderFlags = stream.ReadUInt32();
             header.NumberOfRvaAndSizes = stream.ReadUInt32();
+            header.ExportTable = stream.ReadUInt32();
+            header.SizeOfExportTable = stream.ReadUInt32();
+            header.ImportTable = stream.ReadUInt32();
+            header.SizeOfImportTable = stream.ReadUInt32();
+            header.ResourceTable = stream.ReadUInt32();
+            header.SizeOfResourceTable = stream.ReadUInt32();
+            header.ExceptionTable = stream.ReadUInt32();
+            header.SizeOfExceptionTable = stream.ReadUInt32();
+            header.CertificateTable = stream.ReadUInt32();
+            header.SizeOfCertificateTable = stream.ReadUInt32();
+            header.BaseRelocationTable = stream.ReadUInt32();
+            header.SizeOfBaseRelocationTable = stream.ReadUInt32();
+            header.Debug = stream.ReadUInt32();
+            header.SizeOfDebug = stream.ReadUInt32();
+            header.ArchitectureData = stream.ReadUInt32();
+            header.SizeOfArchitectureData = stream.ReadUInt32();
+            header.GlobalPtr = stream.ReadUInt32();
+            stream.ReadUInt32(); // Zeroes filled.
+            header.TlsTable = stream.ReadUInt32();
+            header.SizeOfTlsTable = stream.ReadUInt32();
+            header.LoadConfigTable = stream.ReadUInt32();
+            header.SizeOfLoadConfigTable = stream.ReadUInt32();
+            header.BoundImport = stream.ReadUInt32();
+            header.SizeOfBoundImport = stream.ReadUInt32();
+            header.ImportAddressTable = stream.ReadUInt32();
+            header.SizeOfImportAddressTable = stream.ReadUInt32();
+            header.DelayImportDescriptor = stream.ReadUInt32();
+            header.SizeOfDelayImportDescriptor = stream.ReadUInt32();
+            header.ClrRuntimeHJeader = stream.ReadUInt32();
+            header.SizeOfClrRuntimeHJeaderr = stream.ReadUInt32();
 
             return header;
         }
