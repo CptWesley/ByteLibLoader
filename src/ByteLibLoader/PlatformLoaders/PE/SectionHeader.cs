29a0600b97778af857c4c5cc39c55b9f6a24547f
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-14 12:45:51 +0100

    Add section header classes

---
+++ src/ByteLibLoader/PlatformLoaders/PE/SectionHeader.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using ExtensionNet.Streams;
+
+namespace ByteLibLoader.PlatformLoaders.PE
+{
+    /// <summary>
+    /// Represents a parsed section header in a PE file.
+    /// </summary>
+    public class SectionHeader
+    {
+        /// <summary>
+        /// Gets or sets the name of the section.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the virtual size of the section.
+        /// </summary>
+        public uint VirtualSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the virtual address of the section.
+        /// </summary>
+        public uint VirtualAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the raw data of the section.
+        /// </summary>
+        public uint SizeOfRawData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pointer to the raw data of the section.
+        /// </summary>
+        public uint PointerToRawData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pointer to the relocations table of the section.
+        /// </summary>
+        public uint PointerToRelocations { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pointer to the line number table of the section.
+        /// </summary>
+        public uint PointerToLinenumbers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the relocation table of the section.
+        /// </summary>
+        public ushort NumberOfRelocations { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the line number table of the section.
+        /// </summary>
+        public ushort NumberOfLinenumbers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the characteristics of the section.
+        /// </summary>
+        public SectionHeaderCharacteristics Characteristics { get; set; }
+
+        /// <summary>
+        /// Reads a section header from the stream.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <returns>The section header read from stream.</returns>
+        public static SectionHeader FromStream(Stream stream)
+            => new SectionHeader()
+            {
+                Name = stream.ReadString(8),
+                VirtualSize = stream.ReadUInt32(),
+                VirtualAddress = stream.ReadUInt32(),
+                SizeOfRawData = stream.ReadUInt32(),
+                PointerToRawData = stream.ReadUInt32(),
+                PointerToRelocations = stream.ReadUInt32(),
+                PointerToLinenumbers = stream.ReadUInt32(),
+                NumberOfRelocations = stream.ReadUInt16(),
+                NumberOfLinenumbers = stream.ReadUInt16(),
+                Characteristics = (SectionHeaderCharacteristics)stream.ReadUInt32()
+            };
+    }
+}
---
+++ src/ByteLibLoader/PlatformLoaders/PE/SectionHeaderCharacteristics.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ByteLibLoader.PlatformLoaders.PE
+{
+    /// <summary>
+    /// Flags for section header characteristics.
+    /// </summary>
+    [Flags]
+    public enum SectionHeaderCharacteristics
+    {
+        /// <summary>
+        /// Indicates that the section can be discarded.
+        /// </summary>
+        MemoryDiscardable = 0x02000000,
+
+        /// <summary>
+        /// Indicates that the section can not be cached.
+        /// </summary>
+        MemoryNotCached = 0x04000000,
+
+        /// <summary>
+        /// Indicates that the section can not be paged.
+        /// </summary>
+        MemoryNotPaged = 0x08000000,
+
+        /// <summary>
+        /// Indicates that the section can be shared.
+        /// </summary>
+        MemoryShared = 0x10000000,
+
+        /// <summary>
+        /// Indicates that the section can be executed as code.
+        /// </summary>
+        MemoryExecute = 0x20000000,
+
+        /// <summary>
+        /// Indicates that the section can be read.
+        /// </summary>
+        MemoryRead = 0x40000000,
+
+        /// <summary>
+        /// Indicates that the section can be written to.
+        /// </summary>
+        MemoryWrite = -2147483648,
+
+        /// <summary>
+        /// Indicates that the section contains executable code.
+        /// </summary>
+        ContainsCode = 0x00000020,
+
+        /// <summary>
+        /// Indicates that the section contains initiliazed data.
+        /// </summary>
+        ContainsInitializedData = 0x00000040,
+
+        /// <summary>
+        /// Indicates that the section contains uninitiliazed data.
+        /// </summary>
+        ContainsUninitializedData = 0x00000080
+    }
+}
