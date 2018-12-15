57f9cb061825604c164e36e07f95035037737d86
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-15 10:26:34 +0100

    Add section loading

--- src/ByteLibLoader/PlatformLoaders/WindowsLibraryLoader.cs
+++ src/ByteLibLoader/PlatformLoaders/WindowsLibraryLoader.cs
@@ -36,9 +36,42 @@ namespace ByteLibLoader.PlatformLoaders
                 {
                     return IntPtr.Zero;
                 }
-            }
 
-            return IntPtr.Zero;
+                IntPtr memory = AllocateImage(optionalHeader.ImageBase, optionalHeader.SizeOfImage);
+                if (memory == IntPtr.Zero)
+                {
+                    return IntPtr.Zero;
+                }
+
+                SectionHeader[] sectionHeaders = new SectionHeader[peHeader.NumberOfSections];
+                for (int i = 0; i < peHeader.NumberOfSections; i++)
+                {
+                    SectionHeader sectionHeader = SectionHeader.FromStream(ms);
+                    sectionHeaders[i] = sectionHeader;
+                    uint size = sectionHeader.SizeOfRawData;
+                    if (size == 0)
+                    {
+                        if (sectionHeader.Characteristics.HasFlag(SectionHeaderCharacteristics.ContainsInitializedData))
+                        {
+                            size = optionalHeader.SizeOfInitializedData;
+                        }
+                        else if (sectionHeader.Characteristics.HasFlag(SectionHeaderCharacteristics.ContainsUninitializedData))
+                        {
+                            size = optionalHeader.SizeOfUninitializedData;
+                        }
+                    }
+
+                    IntPtr section = AllocateSection(memory, sectionHeader.VirtualAddress, sectionHeader.SizeOfRawData);
+                    if (section == IntPtr.Zero)
+                    {
+                        return IntPtr.Zero;
+                    }
+
+                    Marshal.Copy(library, (int)sectionHeader.PointerToRawData, section, (int)sectionHeader.SizeOfRawData);
+                }
+
+                return memory;
+            }
         }
 
         /// <inheritdoc/>
@@ -49,8 +82,18 @@ namespace ByteLibLoader.PlatformLoaders
         public IntPtr GetSymbol(IntPtr library, string symbol)
             => NativeMethods.GetProcAddress(library, symbol);
 
+        private IntPtr AllocateImage(ulong imageBase, uint imageSize)
+            => NativeMethods.VirtualAlloc(new IntPtr((long)imageBase), new UIntPtr(imageSize), NativeMethods.AllocationTypeReserve, NativeMethods.MemoryProtectionReadWrite);
+
+        private IntPtr AllocateSection(IntPtr basePointer, ulong virtualAddress, uint size)
+            => NativeMethods.VirtualAlloc(new IntPtr(basePointer.ToInt64() + (long)virtualAddress), new UIntPtr(size), NativeMethods.AllocationTypeCommit, NativeMethods.MemoryProtectionReadWrite);
+
         private static class NativeMethods
         {
+            internal const uint AllocationTypeReserve = 0x2000;
+            internal const uint AllocationTypeCommit = 0x1000;
+            internal const uint MemoryProtectionReadWrite = 0x04;
+
             [DllImport("kernel32")]
             [return: MarshalAs(UnmanagedType.Bool)]
             internal static extern bool FreeLibrary(IntPtr hModule);
@@ -60,6 +103,9 @@ namespace ByteLibLoader.PlatformLoaders
 
             [DllImport("kernel32")]
             internal static extern IntPtr GetProcAddress(IntPtr hModule, [MarshalAs(UnmanagedType.LPStr)]string procName);
+
+            [DllImport("kernel32")]
+            internal static extern IntPtr VirtualAlloc(IntPtr lpAddress, UIntPtr dwSize, uint lAllocationType, uint flProtect);
         }
     }
 }
